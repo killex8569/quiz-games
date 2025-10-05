91e718a7813d8fb01f6bfc8f1fb7c04d7e5a4fb3
killex8569 <alexandreanette@gmail.com>
2025-10-05 16:32:23 +0200

    Feat : Vies, scores, questions avec réponses

--- main.py
+++ main.py
@@ -1,6 +1,47 @@
 from random import *
+import json
+player_score = 0
+player_chance = 3
+
+
+def main_menu():
+    print("Bienvenu dans le Quiz Game ! ")
+
+    # Proposition de la section pour l'utilisateur
+    print("Dans qu'elle section voulez vous vous tester ? : ")
+    print("1 - Astronomie\n2 - Developpement\n3 - Philo\n4 - Other")
+    user_choix_section = int(input("votre choix : "))
+    if user_choix_section == 2:
+        menu_dev()
+
+def menu_dev():
+    global player_score, player_chance
+    with open("qst/qst_dev.json", "r", encoding="utf-8") as f:
+        questions = json.load(f)
+    
+    q = choice(questions)
+    print(q["question"])
+    for letter, options in q["options"].items():
+        print(f"{letter}: {options}")
+
+    user_answer = str(input("Votre réponse : "))
+    if player_chance == 0:
+        print("Vous avez perdu !")
+
+    else : 
+        if user_answer == q["answer"]:
+            print("Oui bien jouer")
+            player_score += 1
+            print("Vies restante(s) : ", player_chance)
+            print("Score actuelle", player_score)
+            menu_dev()
+        else:
+            print("Non, pas cela")
+            player_chance -= 1
+            print("Vies restante(s) : ", player_chance)
+            print("Score actuelle", player_score)
+            menu_dev()
 
 
 
-question = randrange(100)
-print(question)
+main_menu()
